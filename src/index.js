d0bc92fa9df97198b342f3d792c22562415c70f0
arslanbekbolotov <arslanbekbolotov58@gmail.com>
2024-02-10 23:46:30 +0600

    добавил плагин для форматирования

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './index.scss';
+import "./index.scss";
 
-const app = document.querySelector('#root');
-const mainTitle = document.createElement('h1');
+const app = document.querySelector("#root");
+const mainTitle = document.createElement("h1");
 mainTitle.textContent = "Pure Form TEST";
 
 app.append(mainTitle);
