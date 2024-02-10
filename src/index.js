9dc58df70255325dcf81100514a1094236d7c143
arslanbekbolotov <arslanbekbolotov58@gmail.com>
2024-02-10 23:42:12 +0600

    сконфигурировал webpack создал структуру проекта

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.scss';
+
+const app = document.querySelector('#root');
+const mainTitle = document.createElement('h1');
+mainTitle.textContent = "Pure Form TEST";
+
+app.append(mainTitle);
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname,'./src/index.js'),
+    output: {
+        path: path.resolve(__dirname,'./build'),
+        filename: "bundle.main.js",
+        clean: true,
+    },
+    resolve: {
+        alias: {
+            asserts:path.resolve(__dirname,'./src/asserts'),
+            extensions:['.js'],
+        }
+    },
+    devServer: {
+        port:'3000',
+        static:path.resolve(__dirname,'./src'),
+        host:'0.0.0.0'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader:'babel-loader',
+                exclude:/node_modules/,
+                options: {
+                    babelrc:true
+                }
+            },
+            {
+                test: /\.scss$/,
+                use:['style-loader','css-loader','sass-loader']
+            },
+            {
+                test: /\.jpeg$/,
+                type:'asset/resource',
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname,'./public/template.html'),
+            filename: "index.html"
+        })
+    ]
+}
