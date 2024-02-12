ab9c8c4c5a1906774ae05d8728521adf2452c69a
arslanbekbolotov <arslanbekbolotov58@gmail.com>
2024-02-12 14:40:55 +0600

    создал две страницы с формой и модальным окном

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,23 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.js"),
+  entry: path.resolve(__dirname, './src/index.js'),
   output: {
-    path: path.resolve(__dirname, "./build"),
-    filename: "bundle.main.js",
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.main.js',
     clean: true,
   },
-  resolve: {
-    alias: {
-      asserts: path.resolve(__dirname, "./src/asserts"),
-      extensions: [".js"],
-    },
-  },
   devServer: {
-    port: "3000",
-    static: path.resolve(__dirname, "./src"),
-    host: "0.0.0.0",
+    port: '3000',
+    static: path.resolve(__dirname, './src'),
+    host: '0.0.0.0',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
           babelrc: true,
@@ -31,18 +25,22 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.jpeg$/,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./public/template.html"),
-      filename: "index.html",
+      template: path.resolve(__dirname, './public/feedback-form.html'),
+      filename: 'form.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      filename: 'index.html',
     }),
   ],
 };
